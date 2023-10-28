d17b0f68986fce997730f7e415d3dd13184accb1
WoXelll <svyat1619@gmail.com>
2023-10-28 16:11:11 +0300

    alina's code 2 (i believe it works)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,51 +1,56 @@
 package org.firstinspires.ftc.teamcode;
-//VAN 22.10.2023
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.opencv.core.Mat;
 
 @TeleOp
 public class Test extends LinearOpMode {
     private DcMotor DcMotor1;
     private DcMotor DcMotor2;
     private DcMotor DcMotor3;
-    private DcMotor DcMotor4;
+    private DcMotor handUp;
     double motorPower1;
     double motorPower2;
     double motorPower3;
     double power;
-    double limitation;
-    double upperlimit;
-    double lowerlimit;
 
     public void DcMotorPower()
     {
-        double X = gamepad1.left_stick_x;;
-        double Y = gamepad1.left_stick_y;
+        double X = gamepad1.right_stick_x;
+        double Y = gamepad1.right_stick_y;
         motorPower1 = -0.5 * X - Math.sqrt(3)/2 * Y + power;
         motorPower2 = -0.5 * X + Math.sqrt(3)/2 * Y + power;
         motorPower3 = X + power;
         DcMotor1.setPower(motorPower1);
         DcMotor2.setPower(motorPower2);
         DcMotor3.setPower(motorPower3);
-        double rotation = gamepad1.right_stick_x;
-        motorPower1 = rotation;
-        motorPower2 = rotation;
-        motorPower3 = rotation;
-
-
     }
 
-    public void LiftMotor()
-    {
-        double lift = gamepad1.right_stick_y;
-        limitation += lift;
-        if (limitation < upperlimit && limitation > lowerlimit)
-        {
-            DcMotor4.setPower(lift);
+    public void motorTurn(){
+        double x = gamepad1.left_stick_x;
+        DcMotor1.setPower(-x);
+        DcMotor2.setPower(-x);
+        DcMotor3.setPower(-x);
+    }
+
+    public void take(){
+        boolean x = gamepad1.right_bumper;
+        double y=0;
+        if (x == true){
+            y=1;
         }
-
-
+        handUp.setPower(y);
+        boolean x1 = gamepad1.left_bumper;
+        double y1 = 0;
+        if (x1 == true){
+            y1 = -1;
+        }
+        if (y1 != 0.0){
+            handUp.setPower(y1);
+        }
     }
 
 
@@ -54,7 +59,7 @@ public class Test extends LinearOpMode {
         DcMotor1 = hardwareMap.get(DcMotor.class, "DcMotor1");
         DcMotor2 = hardwareMap.get(DcMotor.class, "DcMotor2");
         DcMotor3 = hardwareMap.get(DcMotor.class, "DcMotor3");
-        DcMotor4 = hardwareMap.get(DcMotor.class, "DcMotor4");
+        handUp = hardwareMap.get(DcMotor.class, "handUp");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
@@ -66,7 +71,8 @@ public class Test extends LinearOpMode {
             telemetry.addData("Status", "Running");
             telemetry.update();
             DcMotorPower();
-
+            motorTurn();
+            take();
         }
     }
 }
