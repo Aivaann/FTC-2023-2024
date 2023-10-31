ecb10bb00948176af7b0334c329876a37aa46908
WoXelll <svyat1619@gmail.com>
2023-10-31 23:17:37 +0300

    our's code 3 (it must works)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -56,6 +56,7 @@ public class Test extends LinearOpMode {
 
     public void claw()
     {
+
         double servoUp = gamepad1.right_trigger;
         double servoDown = gamepad1.left_trigger;
         if(servoUp != 0) clawPosition = openClaw;
