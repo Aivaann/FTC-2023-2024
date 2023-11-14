7c32cbf7ab5f625367a49af4ce1769efbe7c4d4f
Aivaann <149227446+Aivaann@users.noreply.github.com>
2023-11-14 16:22:42 +0300

    Wheel code after test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni4wheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni4wheel.java
@@ -3,28 +3,60 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class Omni4wheel extends LinearOpMode
+public class main1 extends LinearOpMode
 {
 
     // задаём моторы, название мотров в коде, изначально они не объявлены
     private DcMotor rightDrive_ass, rightDrive_fr, leftDrive_ass, leftDrive_fr  = null;
     // а так как нам очень скучно, то мы заодно и указываем на то что они не объявлены
+    boolean k = true;
 
     public void motors_setPower()
     { // публичная функция не возвращаяет значения - void
         double axial   = -gamepad1.right_stick_y;
         double lateral =  gamepad1.right_stick_x;
         double yaw     =  gamepad1.left_stick_x;
-        double leftFrontPower  = Math.sin(45) * axial + Math.cos(45) * lateral + yaw;
-        double rightFrontPower = Math.sin(135) * axial - Math.cos(135) * lateral - yaw;
-        double leftBackPower   = Math.sin(225) * axial - Math.cos(225) * lateral + yaw;
-        double rightBackPower  = Math.sin(315) * axial + Math.cos(315) * lateral - yaw;
+        double rightFrontPower;
+        double leftFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+        yaw *= 10;
+        if ((lateral <= 0.1 && lateral > 0)  (lateral >= -0.1 && lateral < 0)){
+            rightFrontPower  = -Math.sin(45) * axial - yaw;
+            leftFrontPower = Math.sin(45) * axial - yaw;
+            leftBackPower   = -Math.sin(45) * axial - yaw;
+            rightBackPower  = Math.sin(45) * axial - yaw;
+        }else if ((axial <= 0.1 && axial > 0)  (axial >= -0.1 && axial < 0)){
+            rightFrontPower  = - Math.sin(45) * lateral - yaw;
+            leftFrontPower = Math.sin(45) * lateral - yaw;
+            leftBackPower   = Math.sin(45) * lateral - yaw;
+            rightBackPower  = - Math.sin(45) * lateral - yaw;
+        }else{
+            rightFrontPower  = -Math.sin(45) * axial - Math.sin(45) * lateral - yaw;
+            leftFrontPower = Math.sin(45) * axial + Math.sin(45) * lateral - yaw;
+            leftBackPower   = -Math.sin(45) * axial + Math.sin(45) * lateral - yaw;
+            rightBackPower  = Math.sin(45) * axial - Math.sin(45) * lateral - yaw;
+        }
+        rightFrontPower *=2;
+        leftFrontPower *=2;
+        leftBackPower *=2;
+        rightBackPower *=2;
+        if (gamepad1.square){
+            k=!k;
+        }
+        if (!k){
+            rightFrontPower = rightFrontPower/Math.abs(rightFrontPower)*Math.min(Math.abs(rightFrontPower)/4, 0.5);
+            leftFrontPower = leftFrontPower/Math.abs(leftFrontPower) * Math.min(Math.abs(leftFrontPower)/4, 0.5);
+            leftBackPower = leftBackPower/Math.abs(leftBackPower)*Math.min(Math.abs(leftBackPower)/4, 0.5);
+            rightBackPower = rightBackPower/Math.abs(rightBackPower)*Math.min(Math.abs(rightBackPower)/4, 0.5);
+        }
         leftDrive_ass.setPower(leftBackPower); // подаём мощность на моторы
-        leftDrive_fr.setPower(rightFrontPower);
+        leftDrive_fr.setPower(leftFrontPower);
         rightDrive_ass.setPower(rightBackPower);
-        rightDrive_fr.setPower(leftFrontPower);
+        rightDrive_fr.setPower(rightFrontPower);
     }
 
     //начнём оснавную часть
@@ -38,17 +70,15 @@ public class Omni4wheel extends LinearOpMode
         leftDrive_ass = hardwareMap.get(DcMotor.class, "left_ass");
         rightDrive_fr = hardwareMap.get(DcMotor.class, "right_fr");
         rightDrive_ass = hardwareMap.get(DcMotor.class, "right_ass");
-        leftDrive_fr.setDirection(DcMotor.Direction.FORWARD); // куда изначально крутятся моторы, тк моторы могут быть направлены в разные стороны
-        leftDrive_ass.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive_fr.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive_ass.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive_fr.setDirection(DcMotor.Direction.REVERSE); // куда изначально крутятся моторы, тк моторы могут быть направлены в разные стороны
+        leftDrive_ass.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive_fr.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive_ass.setDirection(DcMotor.Direction.REVERSE);
         waitForStart(); // ожидаем запуск программы
         //runtime.reset();
 
         while (opModeIsActive()) {
-
             motors_setPower(); // вызываем функцию, передаём значения с джойстика
-
             telemetry.addData("Enc_left", leftDrive_fr.getCurrentPosition()); //вывод телеметрии
             //telemetry.addData("Status", "Run Time: " + runtime.toString()); // вывод телеметрии
             telemetry.update(); // обновление
